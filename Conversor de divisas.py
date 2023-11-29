e6f5edd43e39c7c7169551158ed17cb7b828d3a7
Alfajor-616 <145789823+Alfajor-616@users.noreply.github.com>
2023-11-28 23:03:36 -0500

    Update Conversor de divisas.py

--- Conversor de divisas.py
+++ Conversor de divisas.py
@@ -9,75 +9,92 @@ from threading import Thread
 response = requests.get(
     'https://free.currconv.com/api/v7/currencies?apiKey=98f5e93c8159297df987')
 currencies = response.json()['results']
-# se convierte a una lista las llaves obtenidas
+# Se convierte a una lista las llaves obtenidas
 currency_l = list(currencies.keys())
-# se organiza la lista con el metodo sorted
+# Se organiza la lista con el metodo sorted
 currency_list = sorted([elemento for elemento in currency_l])
 
 # Crear la ventana principal
-root = tk.Tk()
+ventana = tk.Tk()
 # se le da el titulo a la ventana principal
-root.title("Convertidor de Divisas")
+ventana.title("Convertidor de Divisas")
 
 # Crear ComboBox para la moneda de origen
-label_origen = ttk.Label(root, text="Moneda de origen:")
+label_origen = ttk.Label(ventana, text="Moneda de origen:")
 label_origen.pack()
-combo_origen = ttk.Combobox(root, values=currency_list)
+combo_origen = ttk.Combobox(ventana, values=currency_list)
 combo_origen.pack()
 
 # Crear ComboBox para la moneda de destino
-label_destino = ttk.Label(root, text="Moneda de destino:")
+label_destino = ttk.Label(ventana, text="Moneda de destino:")
 label_destino.pack()
-combo_destino = ttk.Combobox(root, values=currency_list)
+combo_destino = ttk.Combobox(ventana, values=currency_list)
 combo_destino.pack()
 
-# Crear función para realizar la conversión
+# Crear funciónes para realizar la conversión y la grafica
+
+
 def conversion():
+    # Extraer datos de los widgets de la interfaz gráfica
     origen = combo_origen.get()
     destino = combo_destino.get()
     cantidad = float(entry_cantidad.get())
+
+    # Enviar solicitud a la API
     url = f'https://free.currconv.com/api/v7/convert?q={origen}_{destino},{destino}_{origen}&compact=ultra&apiKey=98f5e93c8159297df987'
+
     response = requests.get(url)
     data = response.json()
+    # Recuperar la tasa de conversión
     tasa_conversion = data[f'{origen}_{destino}']
+    # Calcular el resultado
     resultado = cantidad * tasa_conversion
+    # Actualizar la etiqueta con el resultado
     label_resultado.config(text=f"Resultado: {resultado} {destino}")
 
 
-# Crear campos de entrada y botón para realizar la conversión
-label_cantidad = ttk.Label(root, text="Cantidad a convertir:")
+def graficar_en_front(origen, destino):
+
+    url_fechas = f'https://free.currconv.com/api/v7/convert?q={origen}_{destino},{destino}_{origen}&compact=ultra&date=2023-11-20&endDate=2023-11-28&apiKey=98f5e93c8159297df987'
+
+    # Obtener datos desde la URL
+    response_fechas = requests.get(url_fechas)
+    data1 = response_fechas.json()
+
+    # Extraer fechas y valores de los datos
+    fechas = list(data1[f'{origen}_{destino}'].keys())
+    valores = list(data1[f'{origen}_{destino}'].values())
+
+    # Crear un gráfico con fechas en el eje x y valores en el eje y
+    fig, ax = plt.subplots()
+    ax.plot(fechas, valores)
+
+    # Configurar las etiquetas del eje x del gráfico
+    ax.tick_params(axis='x', labelsize=6)
+
+    # Establecer etiquetas y título para el gráfico
+    ax.set_xlabel('Fecha')
+    ax.set_ylabel('Valor')
+    ax.set_title(f'{origen}_{destino}')
+
+    # Mostrar el gráfico en la interfaz gráfica
+    canvas = FigureCanvasTkAgg(fig, master=ventana)
+    canvas.get_tk_widget().pack()
+    canvas.draw()
+    canvas.flush_events()
+
+
+# Crear campos de entrada y botón para realizar la conversión y crear boton para mostrar el grafico
+label_cantidad = ttk.Label(ventana, text="Cantidad a convertir:")
 label_cantidad.pack()
-entry_cantidad = ttk.Entry(root)
+entry_cantidad = ttk.Entry(ventana)
 entry_cantidad.pack()
-button_convertir = ttk.Button(root, text="Convertir", command=conversion)
+button_convertir = ttk.Button(ventana, text="Convertir", command=conversion)
 button_convertir.pack()
-
-# Crear el apartado de resultados
-label_resultado = ttk.Label(root, text="Resultado:")
+label_resultado = ttk.Label(ventana, text="Resultado:")
 label_resultado.pack()
+button_graficar = ttk.Button(ventana, text="Mostrar Gráfico", command=lambda: graficar_en_front(
+    combo_origen.get(), combo_destino.get()))
+button_graficar.pack()
 
-# Crear gráfica de fluctuación de la moneda
-fig, ax = plt.subplots()
-#  datos y configurar la gráfica según tus necesidades
-
-# ax.plot(x_data, y_data)
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.draw()
-canvas.get_tk_widget().pack()
-
-root.mainloop()
-
-# Función para realizar la solicitud HTTP en segundo plano
-def get_currencies():
-    global currencies
-    response = requests.get(
-        'https://free.currconv.com/api/v7/currencies?apiKey=98f5e93c8159297df987')
-    currencies = response.json()['results']
-
-# Función para realizar la conversión en segundo plano
-def perform_conversion():
-    Thread(target=get_currencies).start()
-    Thread(target=conversion).start()
-
-
-root.mainloop()
+ventana.mainloop()
